132c45e46cf6c0905dc05241c3ee4535e32db07e
Xtrah <xtrah@live.com>
2020-10-09 12:04:27 +0200

    test: add exampleTest for fxui #18

---
+++ logger/fxui/src/test/java/logger/fxui/AppTest.java
@@ -0,0 +1,12 @@
+package logger.fxui;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    void exampleTest() {
+        Assertions.assertTrue(true);
+    }
+}
