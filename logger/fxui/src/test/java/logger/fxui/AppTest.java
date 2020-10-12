76c7df2e3e066c69e715e43a22633bbfa4680231
Jørgen Reimers <jorgenreim@gmail.com>
2020-10-12 13:06:42 +0200

    fix: fix fxui report

--- logger/fxui/src/test/java/logger/fxui/AppTest.java
+++ logger/fxui/src/test/java/logger/fxui/AppTest.java
@@ -1,12 +1,49 @@
 package logger.fxui;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import logger.fxui.AppController;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
 
-public class AppTest {
+public class AppTest extends ApplicationTest {
+    private AppController controller;
+
+    @Override
+    public void start(final Stage stage) throws Exception {
+        final FXMLLoader loader = new FXMLLoader(getClass().getResource("App.fxml"));
+        final Parent root = loader.load();
+        this.controller = loader.getController();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 
     @Test
-    void exampleTest() {
-        Assertions.assertTrue(true);
+    public void testNameInput() {
+        TextField inputName = lookup("#inputName").query();
+        String text = "Ola50 Nordmann2020";
+        clickOn("#inputName").write(text);
+        Assertions.assertEquals("Ola Nordmann", inputName.getText());
+    }
+
+    @Test
+    public void testPhoneInput1() {
+        TextField inputPhone = lookup("#inputPhone").query();
+        String text = "99rgk99rgkrg99rgk99";
+        clickOn("#inputPhone").write(text);
+        Assertions.assertEquals("99999999", inputPhone.getText());
+    }
+
+    @Test
+    public void testPhoneInput2() {
+        TextField inputPhone = lookup("#inputPhone").query();
+        String text = "99rgk99rgkrg99rgk99";
+        clickOn("#inputPhone").write(text);
+        Assertions.assertEquals("99999999", inputPhone.getText());
     }
 }
