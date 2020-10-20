23f3f1b3502d807d3e2092f609f3bc7c3755d184
Jørgen Reimers <jorgenreim@gmail.com>
2020-10-20 11:16:33 +0200

    test: remove unused test for VisitLog and add relevant tests. #37

--- logger/core/src/test/java/logger/core/VisitLogTest.java
+++ logger/core/src/test/java/logger/core/VisitLogTest.java
@@ -13,61 +13,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class VisitLogTest {
 
-    private static final String FILEPATH = "log.json";
-
     private VisitLog log;
+    private Visit v1;
+    private Visit v2;
 
     @BeforeEach
     void setUp() {
-        Visit v1 = new Visit("Ola Normann", "12345678", "Realfagsbygget", "A4-100",
+        v1 = new Visit("Ola Normann", "12345678", "Realfagsbygget", "A4-100",
                 LocalDateTime.of(2020, 10, 1, 14, 15),
                 LocalDateTime.of(2020, 10, 1, 16, 15));
-        Visit v2 = new Visit("Kari Hansen", "487654321", "Stripa", "S4",
+        v2 = new Visit("Kari Hansen", "487654321", "Stripa", "S4",
                 LocalDateTime.of(2020, 10, 1, 16, 15),
                 LocalDateTime.of(2020, 10, 1, 18, 15));
 
         log = new VisitLog();
+    }
+
+    @Test
+    void testAddVisit() {
+        assertEquals(0, log.getLog().size());
+
         log.addVisit(v1);
+        assertEquals(1, log.getLog().size());
+
         log.addVisit(v2);
-    }
-
-    @AfterEach
-    void tearDown() {
-        File file = new File(FILEPATH);
-        if (file.delete())
-            System.out.println("File deleted successfully");
-        else {
-            System.out.println("Couldn't delete file");
-            Assertions.fail();
-        }
-    }
-
-    @Test
-    void writeToFile() {
-        log.writeToFile(FILEPATH);
-
-        File file = new File(FILEPATH);
-        Assertions.assertTrue(file.exists());
-    }
-
-    @Test
-    void readFromFile() {
-        List<Visit> beforeReadLog = log.getLog();
-        log.writeToFile(FILEPATH);
-        log.readFromFile(FILEPATH);
-
-        assertEquals(beforeReadLog.size(), log.getLog().size());
-    }
-
-    @Test
-    void addVisit() {
-        log.writeToFile(FILEPATH);
         assertEquals(2, log.getLog().size());
-        log.addVisit(new Visit("Brora Hansen", "18273645", "Realfagsbygget", "R1",
-                LocalDateTime.of(2020, 11, 1, 18, 15),
-                LocalDateTime.of(2020, 11, 1, 19, 15)));
-        log.writeToFile(FILEPATH);
-        log.readFromFile(FILEPATH);
-        assertEquals(3, log.getLog().size());
     }
 }
