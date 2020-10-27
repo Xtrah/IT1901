ad148d67a585b9d729d3a5de74e1d6ef96107942
Jørgen Reimers <jorgenreim@gmail.com>
2020-10-27 18:17:51 +0100

    chore: cleanup controller

--- logger/core/src/main/java/logger/core/Visit.java
+++ logger/core/src/main/java/logger/core/Visit.java
@@ -1,7 +1,6 @@
 package logger.core;
 
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 
 public class Visit {
 
--- logger/fxui/src/main/java/logger/fxui/AppController.java
+++ logger/fxui/src/main/java/logger/fxui/AppController.java
@@ -17,7 +17,7 @@ public class AppController {
     private VisitLog log;
     private VisitLogPersistence persistence;
 
-
+    // Registration
     @FXML private TextField inputName;
     @FXML private TextField inputPhone;
     @FXML private ChoiceBox<String> dropdownBuilding;
@@ -30,8 +30,8 @@ public class AppController {
     @FXML private Button buttonRegister;
     @FXML private Label helperText;
 
-    // Visit Log
-    @FXML private TableView tableView;
+    // Log
+    @FXML private TableView<Visit> tableView;
     @FXML private TextField searchField;
     @FXML private ChoiceBox<String> chooseSearch;
     @FXML private Label logFromDateLabel;
@@ -39,98 +39,18 @@ public class AppController {
     @FXML private Label logToDateLabel;
     @FXML private DatePicker logToDate;
 
-    private void resetInputs() {
-        inputName.setText("");
-        inputPhone.setText("");
-        dropdownBuilding.getSelectionModel().clearSelection();
-        dropdownRoom.getSelectionModel().clearSelection();
-        inputDate.setValue(LocalDate.now());
-        inputHour1.setText("");
-        inputMin1.setText("");
-        inputHour2.setText("");
-        inputMin2.setText("");
-        buttonRegister.setDisable(true);
-    }
-
-    private void forceNumberInput(TextField fxidName, int maxLength) {
-        fxidName.textProperty().addListener((observable, oldValue, newValue) -> {
-            // Only allow digits
-            if (!newValue.matches("\\d*")) {
-                fxidName.setText(newValue.replaceAll("[^\\d]", ""));
-            }
-            // Only allow 2 digits
-            if (newValue.length() > maxLength) {
-                fxidName.setText(oldValue);
-            }
-        });
-    }
-
-    private void forceCharacterInput(TextField fxidName) {
-        fxidName.textProperty().addListener((observable, oldValue, newValue) -> {
-            if (!newValue.matches("^[a-zA-ZæøåÆØÅ ]*$")) {
-                fxidName.setText(newValue.replaceAll("[^a-zA-ZæøåÆØÅ ]*$", ""));
-            }
-        });
-    }
-
     @FXML
     void initialize() {
-
         buttonRegister.setDisable(true);
+        initTable();
+        setUpPersistence();
+        setUpFiltering();
+        activateInputRules();
 
-        // Adding listeners to time-inputs;
-        forceNumberInput(inputHour1, 2);
-        forceNumberInput(inputHour2, 2);
-        forceNumberInput(inputMin1, 2);
-        forceNumberInput(inputMin2, 2);
-        forceNumberInput(inputPhone, 8);
-        forceCharacterInput(inputName);
-
-        // DUMMY-INFO for choice boxes
+        // DUMMY-INFO
         dropdownBuilding.getItems().addAll(FXCollections.observableArrayList("Bygg1", "Bygg2"));
         dropdownRoom.getItems().addAll(FXCollections.observableArrayList("Rom1", "Rom2"));
-
-        // Set date to today by default
         inputDate.setValue(LocalDate.now());
-        
-        // For Visit log 
-        // Make column
-        TableColumn<String, Visit> nameCol = new TableColumn<>("Name");
-        // Listen to value 'name' in class 'Visit'
-        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
-        nameCol.setMaxWidth(40);
-
-        TableColumn<String, Visit> phoneCol = new TableColumn<>("Phone");
-        phoneCol.setCellValueFactory(new PropertyValueFactory<>("phone"));
-        phoneCol.setMaxWidth(40);
-
-        TableColumn<String, Visit> buildingCol = new TableColumn<>("Building");
-        buildingCol.setCellValueFactory(new PropertyValueFactory<>("building"));
-        buildingCol.setMaxWidth(60);
-
-        TableColumn<String, Visit> roomCol = new TableColumn<>("Room");
-        roomCol.setCellValueFactory(new PropertyValueFactory<>("room"));
-        roomCol.setMaxWidth(40);
-
-        TableColumn<LocalDateTime, Visit> fromTimeCol = new TableColumn<>("From date");
-        fromTimeCol.setCellValueFactory(new PropertyValueFactory<>("from"));
-        fromTimeCol.setMaxWidth(65);
-
-        TableColumn<LocalDateTime, Visit> toTimeCol = new TableColumn<>("To date");
-        toTimeCol.setCellValueFactory(new PropertyValueFactory<>("to"));
-        toTimeCol.setMaxWidth(60);
-
-        // Add all columns to tableView
-        tableView.getColumns().addAll(nameCol, phoneCol,
-                buildingCol, roomCol,fromTimeCol, toTimeCol);
-
-        persistence = new VisitLogPersistence();
-        log = persistence.readVisitLog();
-        updateTable();
-
-        // Add dropdown alternatives to filter
-        chooseSearch.getItems().addAll(FXCollections.observableArrayList("Name", "Phone", "Building", "Room", "Date"));
-        chooseSearch.getSelectionModel().selectFirst();
 
         System.out.println("Initialized!");
     }
@@ -204,22 +124,8 @@ public class AppController {
 
     }
 
-    private boolean isEmptyString(String str) {
-        return (str == null || str.trim().isEmpty());
-    }
-
-    private boolean lackingValues () {
-        return (
-                isEmptyString(inputName.getText())
-                || isEmptyString(inputPhone.getText())
-                || isEmptyString(inputPhone.getText())
-                || isEmptyString(dropdownBuilding.getValue())
-                || isEmptyString(dropdownRoom.getValue())
-                || inputDate.getValue() == null
-        );
-    }
-
-    @FXML private void filterVisitLog() {
+    @FXML
+    void filterVisitLog() {
         String searchInput = searchField.getText().toLowerCase(); // User input. Case insensitive
         String searchKey = chooseSearch.getValue(); // DropDown choice
         List<Visit> allVisits = log.getLog();
@@ -245,11 +151,112 @@ public class AppController {
         tableView.getItems().addAll(result);
     }
 
+    private void resetInputs() {
+        inputName.setText("");
+        inputPhone.setText("");
+        dropdownBuilding.getSelectionModel().clearSelection();
+        dropdownRoom.getSelectionModel().clearSelection();
+        inputDate.setValue(LocalDate.now());
+        inputHour1.setText("");
+        inputMin1.setText("");
+        inputHour2.setText("");
+        inputMin2.setText("");
+        buttonRegister.setDisable(true);
+    }
 
+    private void forceNumberInput(TextField fxidName, int maxLength) {
+        fxidName.textProperty().addListener((observable, oldValue, newValue) -> {
+            // Only allow digits
+            if (!newValue.matches("\\d*")) {
+                fxidName.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+            // Only allow 2 digits
+            if (newValue.length() > maxLength) {
+                fxidName.setText(oldValue);
+            }
+        });
+    }
+
+    private void forceCharacterInput(TextField fxidName) {
+        fxidName.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("^[a-zA-ZæøåÆØÅ ]*$")) {
+                fxidName.setText(newValue.replaceAll("[^a-zA-ZæøåÆØÅ ]*$", ""));
+            }
+        });
+    }
+
+    private boolean isEmptyString(String str) {
+        return (str == null || str.trim().isEmpty());
+    }
+
+    private boolean lackingValues () {
+        return (
+                isEmptyString(inputName.getText())
+                || isEmptyString(inputPhone.getText())
+                || isEmptyString(inputPhone.getText())
+                || isEmptyString(dropdownBuilding.getValue())
+                || isEmptyString(dropdownRoom.getValue())
+                || inputDate.getValue() == null
+        );
+    }
 
     private void updateTable() {
         persistence.writeVisitLog(log);
         tableView.getItems().clear();
         tableView.getItems().addAll(log.getLog());
+    }
+
+    private void initTable() {
+        // For Visit log
+        // Make column
+        TableColumn<Visit, String> nameCol = new TableColumn<>("Name");
+        // Listen to value 'name' in class 'Visit'
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        nameCol.setMaxWidth(120);
+
+        TableColumn<Visit, String> phoneCol = new TableColumn<>("Phone");
+        phoneCol.setCellValueFactory(new PropertyValueFactory<>("phone"));
+        phoneCol.setMaxWidth(120);
+
+        TableColumn<Visit, String> buildingCol = new TableColumn<>("Building");
+        buildingCol.setCellValueFactory(new PropertyValueFactory<>("building"));
+        buildingCol.setMaxWidth(120);
+
+        TableColumn<Visit, String> roomCol = new TableColumn<>("Room");
+        roomCol.setCellValueFactory(new PropertyValueFactory<>("room"));
+        roomCol.setMaxWidth(120);
+
+        TableColumn<Visit, LocalDateTime> fromTimeCol = new TableColumn<>("From");
+        fromTimeCol.setCellValueFactory(new PropertyValueFactory<>("from"));
+        fromTimeCol.setMaxWidth(200);
+
+        TableColumn<Visit, LocalDateTime> toTimeCol = new TableColumn<>("To");
+        toTimeCol.setCellValueFactory(new PropertyValueFactory<>("to"));
+        toTimeCol.setMaxWidth(200);
+
+        // Add all columns to tableView
+        tableView.getColumns().addAll(nameCol, phoneCol,
+                buildingCol, roomCol,fromTimeCol, toTimeCol);
+
+    }
+
+    private void activateInputRules() {
+        forceNumberInput(inputHour1, 2);
+        forceNumberInput(inputHour2, 2);
+        forceNumberInput(inputMin1, 2);
+        forceNumberInput(inputMin2, 2);
+        forceNumberInput(inputPhone, 8);
+        forceCharacterInput(inputName);
+    }
+
+    private void setUpPersistence() {
+        persistence = new VisitLogPersistence();
+        log = persistence.readVisitLog();
+        updateTable();
+    }
+
+    private void setUpFiltering() {
+        chooseSearch.getItems().addAll(FXCollections.observableArrayList("Name", "Phone", "Building", "Room", "Date"));
+        chooseSearch.getSelectionModel().selectFirst();
     }
 }
