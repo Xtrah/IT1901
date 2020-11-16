efc1b32f36b99ba48261e936414bfb45b90a7e30
Jørgen Reimers <jorgenreim@gmail.com>
2020-11-16 14:32:03 +0100

    test: change behaviour of a test. !52

--- logger/core/src/test/java/logger/json/VisitLogPersistenceTest.java
+++ logger/core/src/test/java/logger/json/VisitLogPersistenceTest.java
@@ -4,15 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.io.File;
-import java.time.LocalDateTime;
-import logger.core.Visit;
 import logger.core.VisitLog;
 import org.junit.jupiter.api.Test;
 
 class VisitLogPersistenceTest {
 
   private final VisitLogPersistence persistence = new VisitLogPersistence(
-      new File(getClass().getResource("sampleLog.json").getFile()));
+      new File(getClass().getResource("sampleLog.json").getPath()));
 
   @Test
   VisitLog readVisitLog() {
@@ -24,16 +22,8 @@ class VisitLogPersistenceTest {
 
   @Test
   void writeVisitLog() {
-    VisitLog toWrite = readVisitLog();
-    Visit toAdd = new Visit("Mats von Bausher",
-        "87128734",
-        "Realfagbygget",
-        "R7",
-        LocalDateTime.of(2020, 10, 31, 12, 15),
-        LocalDateTime.of(2020, 10, 31, 14, 15));
-    toWrite.addVisit(toAdd);
+    VisitLog toWrite = persistence.readVisitLog();
     persistence.writeVisitLog(toWrite);
-    VisitLog afterAdd = persistence.readVisitLog();
-    assertEquals(4, afterAdd.getLog().size());
+    assertEquals(3, persistence.readVisitLog().getLog().size());
   }
 }
